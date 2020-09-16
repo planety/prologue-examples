89ae6f6030a300dd68b90e5eee48eff2ef7ed18e
Innokentiy Sokolov <keshon@zoho.com>
2020-09-16 15:28:34 +0300

    added Readme

--- 3_routes_simple/example.nim
+++ 3_routes_simple/example.nim
@@ -12,7 +12,10 @@ proc selectByParam(ctx: Context) {.async.} =
     resp "This page accessed by URL param: " & ctx.getPathParams("param")
 
 proc selectByDefaultParam(ctx: Context) {.async.} =
-    resp "This page accessed by URL non-existing param - using default one: " & ctx.getPathParams("non_exsiting_param", "defaultParam")
+    resp "This page accessed by URL non-existing param - using default value: " & ctx.getPathParams("non_exsiting_param", "default value")
+
+proc selectMultipleParams(ctx: Context) {.async.} =
+    resp "This page accessed by multiple URL params: " & ctx.getPathParams("param_1", "default value 1") & " and " & ctx.getPathParams("param_2", "default value 2")
 
 
 # Create new (default) settings for Prologue
@@ -26,6 +29,8 @@ var app = newApp(settings = settings)
 app.addRoute("/", index)
 app.addRoute("/nested/url/o/m/g", nested)
 app.addRoute("/select/{param}", selectByParam)
+app.addRoute("/select-wrong/{param}", selectByDefaultParam)
+app.addRoute("/select-multiple/{param_1}/{param_2}", selectMultipleParams)
 
 # Run the instance
 app.run()
